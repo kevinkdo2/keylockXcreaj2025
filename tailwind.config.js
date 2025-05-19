708bb4966380251f90189261b91c32764c56b800
Giselle Serrano <lgiselleserr@gmail.com>
2025-05-19 14:56:11 -0600

    agregué diseño en el index e instalé tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/**/*.ejs"],
+  theme: {
+    extend: {
+      colors: {
+        'azuloscuro': '#2E4EAC ',
+        'azulclaro': '#87B3FA '
+        
+      },
+    },
+  },
+  plugins: [],
+}
